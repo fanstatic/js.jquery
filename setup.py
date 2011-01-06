5c4bdb0e0a158de81ce66b95a3b482c6560a1bc9
Jan-Jaap Driessen <devnull@localhost>
2011-01-06 17:50:22 +0100

    Use version 1.4.4

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-JQUERY_VERSION = '1.4.3'
-version = '1.4.3'
-# Name version after JQUERY_VERSION + .suffix
-
+version = '1.4.4'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
