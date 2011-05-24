0281e4790277c8fcc2adf26b153e978c919e9799
Izhar Firdaus <kagesenshi.87@gmail.com>
2011-05-24 17:00:15 +0800

    Preparing release 1.6.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.6.1dev'
+version = '1.6.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
