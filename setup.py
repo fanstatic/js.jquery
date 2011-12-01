c164ca4bb391ec3435b15951562b4baa51f4ba19
petri <devnull@localhost>
2011-12-01 12:54:49 +0200

    update to jquery 1.7.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.6.2dev'
+version = '1.7.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
