ccaa413e410646daa74df6b76e2e1098ad5f4e44
Lorenzo <l.buonanno@minddistrict.com>
2022-08-29 12:14:37 +0200

    Update to jQuery 3.6.1

--- js/jquery/__init__.py
+++ js/jquery/__init__.py
@@ -4,10 +4,10 @@ library = Library('jquery', 'resources')
 
 jquery = Resource(
     library,
-    'jquery-3.3.1.js',
-    minified='jquery-3.3.1.min.js')
+    'jquery-3.6.1.js',
+    minified='jquery-3.6.1.min.js')
 
 jquery_slim = Resource(
     library,
-    'jquery-3.3.1.slim.js',
-    minified='jquery-3.3.1.slim.min.js')
+    'jquery-3.6.1.slim.js',
+    minified='jquery-3.6.1.slim.min.js')
